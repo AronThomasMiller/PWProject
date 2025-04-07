1a7d8a48dade10514705a4d5f53193b92ed4a2f9
Pyslar Maksym <aronthomasmiller@gmail.com>
2025-04-07 14:36:01 +0300

    after fix

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>PW-final-project</h1>
+
+<h2>This project is a homework assignment that contains automated end-to-end tests using Playwright with TypeScript. The tests verify core functionality on Practice Software Testing, a demo e-commerce website.
+
+The test suite includes 3 automated tests.</h2>
+
+<b>Framework:</b> Playwright Language: TypeScript
+
+<b>Requirements:</b> You should have any git client installed NodeJS 21 or higher VSCode
+
+<b>Install dependencies:</b> npm install , npm i dotenv
+
+<b>Run all tests:</b> npx playwright test 
+
+<b>View the HTML report (after test run):</b> npx playwright show-report
--- tests/firsttests.spec.ts
+++ tests/firsttests.spec.ts
@@ -1,49 +1,45 @@
 import { test, expect } from '@playwright/test';
 
 test('Test 1: Verify login with valid credentials', async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com/auth/login');
-    await page.fill('#email', 'customer@practicesoftwaretesting.com');
-    await page.fill('#password', 'welcome01');
-    await page.click('[data-test="login-submit"]')
-    await page.waitForURL('https://practicesoftwaretesting.com/account');
+    await page.goto(process.env.WEB_URL + '/auth/login');
+    await page.fill('#email', process.env.USER_EMAIL ?? '');
+    await page.fill('#password', process.env.USER_PASSWORD ?? '');
+    await page.getByTestId('login-submit').click();
 
-    await expect(page.url()).toBe('https://practicesoftwaretesting.com/account');
-    await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
-    await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');
+    await expect(page).toHaveURL('/account');
+    await expect(page.getByTestId('page-title')).toContainText('My account');
+    await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
 })
 
 test('Test 2: Verify user can view product details', async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com');
-    await page.locator('[data-test="product-01JR0ARQYP313KEECXVV0B6XFD"]').click()
+    await page.goto(process.env.WEB_URL + '');
+    await page.locator("[class='card']").nth(0).click()
 
-    await expect(page.url()).toContain('https://practicesoftwaretesting.com/product');
-    await expect(page.locator('[data-test="product-name"]')).toContainText('Combination Pliers');
-    await expect(page.locator('[data-test="unit-price"]')).toContainText('14.15');
-    await expect(page.locator('[data-test="add-to-cart"]')).toBeVisible();
-    await expect(page.locator('[data-test="add-to-favorites"]')).toBeVisible();
+    await expect(page).toHaveURL(/\/product\//);
+    await expect(page.getByTestId('product-name')).toContainText('Combination Pliers');
+    await expect(page.getByTestId('unit-price')).toContainText('14.15');
+    await expect(page.getByTestId('add-to-cart')).toBeVisible();
+    await expect(page.getByTestId('add-to-favorites')).toBeVisible();
 })
 
 test('Test 3: Verify user can add product to cart', async ({ page }) => {
-    await page.goto('https://practicesoftwaretesting.com');
-    await page.locator('[data-test="product-01JR0ARQYYG1WAWJYRPSZRAAVV"]').click()
+    await page.goto(process.env.WEB_URL + '');
+    await page.locator("[class='card']").nth(4).click() 
 
-    await expect(page.url()).toContain('https://practicesoftwaretesting.com/product');
-    await expect(page.locator('[data-test="product-name"]')).toContainText('Slip Joint Pliers');
-    await expect(page.locator('[data-test="unit-price"]')).toContainText('9.17');
+    await expect(page).toHaveURL(/\/product\//);
+    await expect(page.getByTestId('product-name')).toContainText('Slip Joint Pliers');
+    await expect(page.getByTestId('unit-price')).toContainText('9.17');
 
-    await page.locator('[data-test="add-to-cart"]').click();
-    
-    await expect(page.getByRole('alert', { name: 'Product added to shopping cart' })).toBeVisible();
+    await page.getByTestId('add-to-cart').click();
+
     await expect(page.getByRole('alert', { name: 'Product added to shopping cart' })).toContainText('Product added to shopping cart');
-    await page.waitForTimeout(8000);
+    await expect(page.getByRole('alert', { name: 'Product added to shopping cart' })).not.toBeVisible({ timeout: 8000 });
     await expect(page.getByRole('alert', { name: 'Product added to shopping cart' })).toBeHidden();
-    await expect(page.locator('[data-test="cart-quantity"]')).toContainText('1');
-    
-    await page.locator('[data-test="nav-cart"]').click();
-    await page.waitForURL('https://practicesoftwaretesting.com/checkout');
+ 
+    await page.getByTestId('nav-cart').click();
 
-    await expect(page.url()).toBe('https://practicesoftwaretesting.com/checkout');
-    await expect(page.locator('[data-test="cart-quantity"]')).toContainText('1');
-    await expect(page.locator('[data-test="product-title"]')).toContainText('Slip Joint Pliers');
-    await expect(page.locator('[data-test="proceed-1"]')).toBeVisible();
+    await expect(page).toHaveURL('/checkout');
+    await expect(page.getByTestId('cart-quantity')).toContainText('1');
+    await expect(page.getByTestId('product-title')).toContainText('Slip Joint Pliers');
+    await expect(page.getByTestId('proceed-1')).toBeVisible();
 })
